6433ccdbde5c61c0e565068be63d975585230b67
TheoMinariniDeMelo <theominarini@outlook.com>
2026-05-21 18:29:33 -0300

    feat: take logical expression from arguments and create README.md

--- main.go
+++ main.go
@@ -13,49 +13,56 @@ func powInt(x, y int) int {
 	return int(math.Pow(float64(x), float64(y)))
 }
 func main(){
-	str := "~p^q|(p^~q|k)";
+	args := os.Args
 
-	var lx *parser.Lexer = parser.NewLexer(str);
+	if len(args) > 2 {
+		fmt.Printf("You must enter just 1 expression")
+		os.Exit(1)
+	}
 
-	var props []string = parser.GetProps(*lx);
+	exp := args[1]
 
-	ast, err := parser.ParseExpression(lx);
+	var lx *parser.Lexer = parser.NewLexer(exp)
+
+	var props []string = parser.GetProps(*lx)
+
+	ast, err := parser.ParseExpression(lx)
 
 	if lxError, ok := err.(parser.UnidentifiedTokenError); ok {
-		fmt.Printf("Invalid expression: %s\n\r%s", str, lxError.Error());
-		os.Exit(1);
+		fmt.Printf("Invalid expression: %s\n\r%s", exp, lxError.Error())
+		os.Exit(1)
 	}
 	if lxError, ok := err.(parser.ParseError); ok {
-		fmt.Printf("Invalid operation: %s\n\r%s", str, lxError.Error());
-		os.Exit(1);
+		fmt.Printf("Invalid operation: %s\n\r%s", exp, lxError.Error())
+		os.Exit(1)
 	}
 
-	l := len(props);
+	l := len(props)
 
-	var maps []map[string]bool = make([]map[string]bool, 1);
+	var maps []map[string]bool = make([]map[string]bool, 1)
 
-	m := make(map[string]bool);
+	m := make(map[string]bool)
 
 	for _, prop := range props {
-		m[prop] = true;
+		m[prop] = true
 	}
 
-	m[str], err = ast.Eval(m);
+	m[exp], err = ast.Eval(m)
 
 
 	if err != nil {
-		fmt.Printf("Invalid operation: %s\n\r%s", str, err.Error());
-		os.Exit(1);
+		fmt.Printf("Invalid operation: %s\n\r%s", exp, err.Error())
+		os.Exit(1)
 	}
 
-	maps[0] = m; 
+	maps[0] = m 
 
-	s := strings.Join(props, " | ");
-	fmt.Print(s + " | " + str + "\n");
+	s := strings.Join(props, " | ")
+	fmt.Print(s + " | " + exp + "\n")
 
 	for i := range powInt(2,l) {
-		m := make(map[string]bool);
-		
+		m := make(map[string]bool)
+
 
 		for j, prop := range props {
 			p := powInt(2, l - (j + 1)) 
@@ -66,13 +73,13 @@ func main(){
 			}
 			fmt.Printf(" %s |", boolToString(m[prop]))
 		}
-		m[str], err = ast.Eval(m);
-		fmt.Printf(" %s |\n", boolToString(m[str]))
-		maps = append(maps, m);
+		m[exp], err = ast.Eval(m)
+		fmt.Printf(" %s |\n", boolToString(m[exp]))
+		maps = append(maps, m)
 
 		if err != nil {
-			fmt.Printf("Invalid operation: %s\n\r%s", str, err.Error());
-			os.Exit(1);
+			fmt.Printf("Invalid operation: %s\n\r%s", exp, err.Error())
+			os.Exit(1)
 		}
 	}
 }
