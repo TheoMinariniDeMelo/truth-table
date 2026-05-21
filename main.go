6b9114cf1571773748fb9a11fa6b2b69f90e6754
TheoMinariniDeMelo <theominarini@outlook.com>
2026-05-21 18:16:27 -0300

    feat: print table

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strings"
+
 	"github.com/TheoMinariniDeMelo/truth-table/parser"
 )
 
+func powInt(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
 func main(){
 	str := "~p^q|(p^~q|k)";
 
 	var lx *parser.Lexer = parser.NewLexer(str);
-	
+
 	var props []string = parser.GetProps(*lx);
 
 	ast, err := parser.ParseExpression(lx);
@@ -24,21 +30,57 @@ func main(){
 		os.Exit(1);
 	}
 
-	result, err := ast.Eval(props);
+	l := len(props);
+
+	var maps []map[string]bool = make([]map[string]bool, 1);
+
+	m := make(map[string]bool);
+
+	for _, prop := range props {
+		m[prop] = true;
+	}
+
+	m[str], err = ast.Eval(m);
+
 
 	if err != nil {
 		fmt.Printf("Invalid operation: %s\n\r%s", str, err.Error());
 		os.Exit(1);
 	}
-	
-	var s string;
 
-	if result {
-		s = "True"
-	} else {
-		s = "False"
+	maps[0] = m; 
+
+	s := strings.Join(props, " | ");
+	fmt.Print(s + " | " + str + "\n");
+
+	for i := range powInt(2,l) {
+		m := make(map[string]bool);
+		
+
+		for j, prop := range props {
+			p := powInt(2, l - (j + 1)) 
+			if i % (p + 1) == p {
+				m[prop] = !maps[i][prop]
+			} else {
+				m[prop] = maps[i][prop]
+			}
+			fmt.Printf(" %s |", boolToString(m[prop]))
+		}
+		m[str], err = ast.Eval(m);
+		fmt.Printf(" %s |\n", boolToString(m[str]))
+		maps = append(maps, m);
+
+		if err != nil {
+			fmt.Printf("Invalid operation: %s\n\r%s", str, err.Error());
+			os.Exit(1);
+		}
 	}
+}
 
-	fmt.Printf("\n\nresult: %s", s);
-
+func boolToString(b bool) string{
+	if b {
+		return "true"
+	} else {
+		return "false"
+	}
 }
